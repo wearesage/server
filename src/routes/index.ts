0020edecf877442ff95fccf668b82ffec16493b8
Zach Winter <zach@Mac.attlocal.net>
2025-05-18 02:32:32 -0400

    Update.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
 import { Router, type Request, type Response } from "express";
 import agentRoutes from "./agent";
 import authRoutes from "./auth";
-import neo4jRoutes from "./neo4j";
+// import neo4jRoutes from "./neo4j";
 import sessionRoutes from "./session";
 const router = Router();
 
@@ -18,7 +18,7 @@ router.get("/status", (req: Request, res: Response) => {
   });
 });
 
-router.use("/neo4j", neo4jRoutes);
+// router.use("/neo4j", neo4jRoutes);
 router.use("/agent", agentRoutes);
 router.use("/auth", authRoutes);
 router.use("/session", sessionRoutes);
